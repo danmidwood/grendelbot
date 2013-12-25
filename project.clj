2f48aabe71289f84285a0a56aedfdbfefbde29ed
Dan Midwood <dan@danmidwood.com>
2013-12-25 02:07:14 -0500

    Specify minimum lein version in project file

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://twitter.com/worldrimwalker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [twitter-api "0.7.4"]
                  [environ "0.4.0"]]
